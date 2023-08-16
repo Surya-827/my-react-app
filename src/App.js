cfc7d5b50fcfee61564eec4d83dbb3d4b32f1dd7
Surya-827 <vsuryakiran827@gmail.com>
2023-08-16 20:31:28 +0530

    Add greet Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+const greetUser = () => {
+  return <marquee>Welcome to REACT</marquee>
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello! Welcome to REACT
+          Hello! 
         </p>
+        <p>{greetUser()}</p>
       </header>
     </div>
   );
